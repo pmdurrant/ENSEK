cf2b3b400508fd1265a332c6c3f791dc4e97076d
pmdurrant <pmdurrant@users.noreply.github.com>
2023-04-17 14:39:26 +0100

    Add entity Unit Test

---
+++ ENSEKTestAPI/TestProjectAPI/Entities/EntityTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENSEK.Entities.Models;
+
+namespace TestProjectAPI.Entities
+{
+    public class EntityTests
+    {
+        [Fact]
+        public void Create_Entity()
+        {
+            var expected = new MeterReading() { AccountId = "2344",MeterReadingDateTime = DateTime.Parse("2/04/2019 09:24"),MeterReadValue = "1002"};
+
+            MeterReading meterreading = new MeterReading("2344", DateTime.Parse("2/04/2019 09:24"), "1002");
+
+            Assert.Equal(expected.Id, meterreading.Id);
+
+            Assert.Equal(expected.MeterReadValue, meterreading.MeterReadValue);
+
+            Assert.Equal(expected.MeterReadingDateTime, meterreading.MeterReadingDateTime);
+
+
+        }
+    }
+    
+}
