3e98e09359d43e1157a83d7aacf37049fda92690
pmdurrant <pmdurrant@users.noreply.github.com>
2023-04-24 13:59:11 +0100

    cleanup directory

--- sonarqubeweb.bat
+++
@@ -1,9 +0,0 @@
-cd C:\working\ENSEK\ENSEKWeb
-
-dotnet sonarscanner begin /k:"ENSEKWeb" /d:sonar.host.url="http://localhost:9000"  /d:sonar.token="sqp_18344350e6674140dc15c10afc1a0bf3e85d9723"
-
-dotnet build
-
-dotnet sonarscanner end /d:sonar.token="sqp_18344350e6674140dc15c10afc1a0bf3e85d9723"
-
-cd ..
