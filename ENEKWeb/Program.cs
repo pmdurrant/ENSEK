e8456b805e2d83853144e85cb3fbb8bd49c50305
pmdurrant <pmdurrant@users.noreply.github.com>
2023-04-25 11:13:12 +0100

    working Web version

--- ENEKWeb/Program.cs
+++ ENEKWeb/Program.cs
@@ -1,27 +1,12 @@
-// ***********************************************************************
-// Assembly         : ENSEKWeb
-// Author           : pdurr
-// Created          : 04-21-2023
-//
-// Last Modified By : pdurr
-// Last Modified On : 04-22-2023
-// ***********************************************************************
-// <copyright file="Program.cs" company="ENSEKWeb">
-//     Copyright (c) . All rights reserved.
-// </copyright>
-// <summary></summary>
-// ***********************************************************************
-
-
-using System.Security.Cryptography.X509Certificates;
 using ENSEK.Contracts;
-using ENSEK.Repository;
 using ENSEKWeb.Common;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using ENSEK.Repository;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
-System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 var builder = WebApplication.CreateBuilder(args);
+
 var loadpath = Directory.GetCurrentDirectory().ToString() + @"/https/_.officeblox.co.uk-crt.pfx";
 
 try
@@ -30,14 +15,16 @@ try
 
 
     builder.WebHost.UseKestrel(options =>
-    { 
+    {
         options.Listen(IPAddress.Any, 5008);
-        options.Listen(IPAddress.Any, 5009,
-       listenOptions =>
-       {
-           listenOptions.UseHttps(cert);
-       });
+        options.Listen(IPAddress.Any, 7150,
+            listenOptions =>
+            {
+                listenOptions.UseHttps(cert);
+            });
     });
+
+   
 }
 catch
 {
@@ -46,10 +33,8 @@ catch
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
 builder.Services.AddScoped<IMeterReadingRepository, MeterReadingRepository>();
 builder.Services.AddTransient<IStandardHttpClient, StandardHttpClient>();
-
 builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 var app = builder.Build();
 
@@ -57,7 +42,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -67,5 +56,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.UseHttpsRedirection();
+
 app.Run();
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ENSEK
-
+<br>
 SonarQube <br>
 [![Quality gate](http://localhost:9000/api/project_badges/quality_gate?project=ENSEK&token=sqb_3981e2a17fcd6b395155a05a5be4dfe42dc30554)](http://localhost:9000/dashboard?id=ENSEK)
 
